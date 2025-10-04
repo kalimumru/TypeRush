ef78baa4f793efe0114a5be9988908a862bdf1ff
Shinchan 181 <s70210270@gmail.com>
2025-10-04 14:36:19 +0000

    ya bhi tik nhi hai jo sab se phele tha wohi raha ne do

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -2,12 +2,7 @@ import TyperushContainer from '@/components/typerush/typerush-container';
 
 export default function PlayPage() {
   return (
-    <div 
-      className="relative min-h-screen w-full overflow-hidden"
-      style={{
-        backgroundImage: 'linear-gradient(to right top, #0c0a1d, #1d153c, #311d5d, #492281, #6625a7)'
-      }}
-    >
+    <div className="relative min-h-screen w-full bg-background overflow-hidden">
       <div className="relative z-10 flex flex-col min-h-dvh">
           <TyperushContainer />
       </div>
