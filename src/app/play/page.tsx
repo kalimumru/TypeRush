251eaf5720e94bfa379f69dcc853dc8cdf965013
Shinchan 181 <s70210270@gmail.com>
2025-10-04 14:33:13 +0000

    background me #ffbf00,#f2cf7e , #ffe642, #ff7900. combine color add karo

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -2,7 +2,12 @@ import TyperushContainer from '@/components/typerush/typerush-container';
 
 export default function PlayPage() {
   return (
-    <div className="relative min-h-screen w-full bg-background overflow-hidden">
+    <div 
+      className="relative min-h-screen w-full overflow-hidden"
+      style={{
+        backgroundImage: 'linear-gradient(to right, #ffbf00, #f2cf7e, #ffe642, #ff7900)'
+      }}
+    >
       <div className="relative z-10 flex flex-col min-h-dvh">
           <TyperushContainer />
       </div>
